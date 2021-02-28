f2568140df42a849875a4a31883309b142b4df09
Lisandro <lisandro@progresstech.dev>
2021-02-28 18:08:11 +0900

    :rocket: fix variant of ReplaceAndRemove_6-4.py

--- Python/Strings/ReplaceAndRemove_6-4.py
+++ Python/Strings/ReplaceAndRemove_6-4.py
@@ -9,7 +9,7 @@
 # change the 'a's into 'dd's
 
 
-def replace_remove(s: str, size: int) -> [str]:
+def replace_remove(s, size: int) -> [str]:
     # In place removal of characters
     char_to_remove = 'b'
     write_idx, a_count = 0, 0
@@ -23,7 +23,6 @@ def replace_remove(s: str, size: int) -> [str]:
     # In place replacement of characters (backwards in this example)
     curr_idx = write_idx - 1
     write_idx += a_count - 1
-    # final_size = write_idx + 1
     while curr_idx >= 0:
         if s[curr_idx] == 'a':
             s[write_idx-1: write_idx + 1] = 'dd'
@@ -53,22 +52,41 @@ def replace_remove(s: str, size: int) -> [str]:
 # shift string to the right
 # yield index
 
-def telex_encode(c: str) -> str:
+def telex_encode(sentence: list) -> list:
 
-    encodings = dict({".": 'DOT', ',': 'COMMA', '?': 'QUESTION MARK', '!': 'EXCLAMATION MARK'})
-    sentence = [s for s in c]  # isn't this O(n) space complexity? Aren't we making a copy of the string into an list?
+    """
+    # Search for occurrences backwards in the array
+    # When a key char is found, we rotate the array to the right until the index is at the end
+    # Remove the last character in the sentence
+    # Append the new characters to the end -> append has O(1) space complexity this way
+    # Rotate the array to the left by n positions to keep the same index
+    """
+
+    encodings = dict({".": ['D', 'O', 'T'], ',': ['C', 'O', 'M', 'M', 'A'],
+                      '?': ['Q', 'U', 'E', 'S', 'T', 'I', 'O', 'N', ' ', 'M', 'A', 'R', 'K'],
+                      '!': ['E', 'X', 'C', 'L', 'A', 'M', 'A', 'T', 'I', 'O', 'N', ' ', 'M', 'A', 'R', 'K']})
     keys = encodings.keys()
 
-    for key in keys:
-        for i in range(len(sentence)):
-            if sentence[i] == key:
-                sentence[i] = encodings[key]
+    curr_idx = len(sentence) - 1
+    while curr_idx >= 0:
+        if sentence[curr_idx] in keys:
+            key = sentence[curr_idx]
+            sentence = sentence[curr_idx+1:] + sentence[:curr_idx+1]  # rotate array to the right
+            sentence = sentence[:-1]
+            for ch in encodings[key]:
+                sentence.append(ch)  # append new chars -> O(1) space complexity
 
-    return "".join(sentence)
+            rot_left = curr_idx + len(encodings[key])  # rotate array to the left
+            sentence = sentence[-rot_left:] + sentence[:-rot_left]
+
+        curr_idx -= 1
+
+    return sentence
 
 
 a = ['a', 'c', 'd', 'b', 'b', 'c', 'a']
 print(replace_remove(a, size=7))
 
-a = 'It is a wonderful day(!), no?'
+a = ['I', 't', 'i', 's', 'a', '(', '!', ')', 'n', 'o', '?']
 print(telex_encode(a))
+
